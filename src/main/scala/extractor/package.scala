409f7b357ac71c7a448d8e16f57cef0b94434b57
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-02-21 16:34:57 +0100

    fix attribute name

--- src/main/scala/extractor/package.scala
+++ src/main/scala/extractor/package.scala
@@ -2,7 +2,7 @@ import java.net.URL
 
 package object example_1 {
 
-  case class User(firstName: String, lastName: String, shcore: Int)
+  case class User(firstName: String, lastName: String, score: Int)
 
   def advance(xs: List[User]) = xs match {
     case User(_, _, score1) :: User(_, _, score2) :: _ => score1 - score2
